66d8c1bcbccccf5efa714a39d6e6de03cb1d0ee9
yutsick <yts.prof@gmail.com>
2022-02-13 07:58:26 +0200

    conditions and ccles. Lesson15. Done

--- app1/app2.js
+++ app1/app2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt('How much films have you seen?');
+const numberOfFilms = prompt('How much films have you seen?');
 const personaMovieDb = {
   'count': numberOfFilms,
   'movies': {},
@@ -11,9 +11,22 @@ const personaMovieDb = {
 
 
 for (let i = 0; i < 2; i++) {
-  let firstQuestion = prompt('One of the last films you seen', "");
-  let secondQuestion = prompt("Set it's rating", 0);
+  const firstQuestion = prompt('One of the last films you seen', ""),
+    secondQuestion = prompt("Set it's rating", 0);
 
+  if (firstQuestion == '' || firstQuestion == null || secondQuestion == '' || secondQuestion == null || firstQuestion.length > 50) {
+    i--;
+    continue;
+  }
   personaMovieDb.movies[firstQuestion] = secondQuestion;
 }
+
+if (personaMovieDb.count < 10 && personaMovieDb.count != 0 && personaMovieDb.count != null)
+  alert('So little bit');
+else if (personaMovieDb.count >= 10 && personaMovieDb.count <= 30)
+  alert('Classical')
+else if (personaMovieDb.count > 30)
+  alert('Kinoman')
+else
+  alert('Error');
 console.log(personaMovieDb);
